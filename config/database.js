fc25d6bfb44fee549c08a5ec02b38111240e29fb
abhipers <abhinsamp@gmail.com>
2020-04-27 19:46:13 +0530

    send html file from index.js

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 
 const setupDB = () => {
-    mongoose.connect(`mongodb+srv://dbUser:${encodeURIComponent('WinkLab@69')}@cluster0-spmyi.gcp.mongodb.net/test?retryWrites=true&w=majority/book-adoc`)
+    mongoose.connect('mongodb+srv://dbUser:AsimplePasswordMate@cluster0-spmyi.gcp.mongodb.net/test?retryWrites=true&w=majority/book-adoc')
         .then(() => console.log("connected to db"))
         .catch(err => console.log(err))
 }
