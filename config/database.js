bd2197638f44ff1aa2f3ae6441a9b323e2489a19
abhipers <abhinsamp@gmail.com>
2020-04-27 20:00:53 +0530

    removed writeconcern

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 
 const setupDB = () => {
-    mongoose.connect('mongodb+srv://dbUser:AsimplePasswordMate@cluster0-spmyi.gcp.mongodb.net/test?retryWrites=true&w=majority/book-adoc')
+    mongoose.connect('mongodb+srv://dbUser:AsimplePasswordMate@cluster0-spmyi.gcp.mongodb.net/test?retryWrites=true/book-adoc')
         .then(() => console.log("connected to db"))
         .catch(err => console.log(err))
 }
