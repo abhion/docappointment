aef76ecd2888d89fc06bc974eab08ce02a51bab4
abhipers <abhinsamp@gmail.com>
2020-04-28 13:19:40 +0530

    db path

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 
 const setupDB = () => {
-    mongoose.connect('mongodb://localhost:27017/book-a-doc')
+    mongoose.connect('mongodb+srv://dbUser:AsimplePasswordMate@cluster0-spmyi.gcp.mongodb.net/test?retryWrites=true/book-adoc')
         .then(() => console.log("connected to db"))
         .catch(err => console.log(err))
 }
