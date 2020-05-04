24487a2dedd1079cd11a91705ce811bbd67e3d20
abhion <abhi_lash@outlook.com>
2020-05-04 21:21:47 +0530

    secret comment changed

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 
 const setupDB = () => {
-    mongoose.connect('mongodb+srv://dbUser:AsimplePasswordMate@cluster0-spmyi.gcp.mongodb.net/test?retryWrites=true/book-adoc') //changed now 
+    mongoose.connect('mongodb+srv://dbUser:pass@cluster0-spmyi.gcp.mongodb.net/test?retryWrites=true/book-adoc') //changed now 
     
     // mongoose.connect('mongodb://localhost:27017/book-a-doc')
         .then(() => console.log("connected to db"))
