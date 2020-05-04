f0a60d4bbf33f8bb8f3665ecf3201acc9c350216
abhion <abhi_lash@outlook.com>
2020-05-04 21:18:00 +0530

    api comment

--- config/database.js
+++ config/database.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 
 const setupDB = () => {
-    mongoose.connect('mongodb+srv://dbUser:AsimplePasswordMate@cluster0-spmyi.gcp.mongodb.net/test?retryWrites=true/book-adoc')
+    mongoose.connect('mongodb+srv://dbUser:AsimplePasswordMate@cluster0-spmyi.gcp.mongodb.net/test?retryWrites=true/book-adoc') //changed now 
+    
     // mongoose.connect('mongodb://localhost:27017/book-a-doc')
         .then(() => console.log("connected to db"))
         .catch(err => console.log(err))
